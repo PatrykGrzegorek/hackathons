d3099622aa3268f67d50561804aa07062b7d8732
roertbb <robert.banaszak1997@gmail.com>
2020-09-28 19:30:21 +0000

    Deploying to gh-pages from master 2c85dbf89c28f3bd969a08cc929ffe005b51f684

---
+++ precache-manifest.cc4fc5d03fa72a9d6cc509d7c04a2549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07c114ed265a33f5544e91deae9c3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6000dd9e098fe3b38e5",
+    "url": "/static/js/2.a1b8b2dc.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/static/js/2.a1b8b2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a68fc3c6921f786186",
+    "url": "/static/js/main.76283caa.chunk.js"
+  },
+  {
+    "revision": "be73ea4c858c03882469",
+    "url": "/static/js/runtime-main.cf6429e1.js"
+  },
+  {
+    "revision": "913590334df54240fda8c803daf511b8",
+    "url": "/static/media/akai-logo.91359033.svg"
+  },
+  {
+    "revision": "e6701352ff09c2cf9d63771cbb15bbff",
+    "url": "/static/media/logo.e6701352.svg"
+  }
+]);
